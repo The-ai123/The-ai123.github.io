a72067097936ffe82c0988933467d9e8044b6de1
The_ai <sans34678@gmail.com>
2023-10-16 12:05:59 -0400

    Changed animation to css and startin on boxes

---
+++ scripts/animation.js
@@ -0,0 +1,10 @@
+//Underlines an element
+function underline(element) {
+    element.style = "text-decoration:underline";
+}
+
+//Removes underline from an element
+function removeUnderline(element) {
+    element.style = "text-decoration:none";
+
+}
